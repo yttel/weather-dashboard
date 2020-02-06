b288f5d5ea66001acba7c296e138eab2184614c7
BytteL <lettybedard@gmail.com>
2020-02-06 13:42:03 -0500

    testing if JS works from site vs local

--- Assets/script.js
+++ Assets/script.js
@@ -3,12 +3,55 @@
 
 $(function(){
   
-  //  DECLARATIONS 
-  
+  //  DECLARATIONS
+  let recentList = null;
+  let apiKey = "e289f98da0cc0e60eb332ebfc1bf33ee";
+  //let zipCode = 0;
+  //let queryUrl = `api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}`
+
   //  REFERENCES
- 
+  let $search = $("#search");
+  let $recent = $("#recent");
+
   //  FUNCTIONS
 
-  //  EVENT LISTENERS
+  //grabs the weather at location ?with days offset (ie 1 means tomorrow etc)
+  function getWeather(location){
+    return $.get(`api.openweathermap.org/data/2.5/weather?zip=${location},us&appid=${apiKey}`);
+  }
 
+  //displays location in right pane
+  function showThePlace(location){
+    if (location){
+      getWeather(location, 0);      
+    }
+  }
+  
+  //empty and draw recent, show most recent in results
+  function renderAll(){
+    $recent.empty();
+    recentList = JSON.parse(localStorage.getItem("recentList"));
+    
+  }
+
+  function addThis(location){
+    //unshift this location to local storage and left pane
+  }
+
+
+  
+  //  EVENT LISTENERS
+  
+  //search button is pushed, add search term to top of recent list and show this place
+  
+  //clear recent is clicked, empty the recent pane
+  
+  //when recent div is clicked, move that div to the top and show this place
+  
+  $.get(`api.openweathermap.org/data/2.5/weather?zip=03833,us&appid=${apiKey}`)
+    .then(function(response){
+      console.log(response);
+    }, console.log("get error"));
+
+  
 });
